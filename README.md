199d22725b63955130270fe38ae134e026ac6fd4
Quentin Bellanger <hello@quentin-bellanger.com>
2022-08-12 15:18:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# dsfr-navigation-arrows-reproduction
+
+Projet de reproduction d'un bug du [DSFR](https://github.com/GouvernementFR/dsfr).
+
+Pour lancer le projet :
+
+```
+yarn
+yarn dev
+```
+
+Visiter [http://localhost:3000](http://localhost:3000)
---
+++ main.js
@@ -0,0 +1,12 @@
+import '@gouvfr/dsfr/dist/dsfr.min.css'
+import '@gouvfr/dsfr/dist/dsfr.module.min.js'
+import '@gouvfr/dsfr/dist/utility/icons/icons.css'
+
+// import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello</h1>
+//   <a href="#" target="_blank" class="fr-link">Pouet</a>
+// `
+
+// setupCounter(document.querySelector('#counter'))
