427caee426af1c80a63eb55ee0f506ee0ceca043
Quentin Bellanger <hello@quentin-bellanger.com>
2023-03-22 16:28:23 +0100

    remove unnecessary stuff

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-import '@gouvfr/dsfr/dist/dsfr.min.css'
-import '@gouvfr/dsfr/dist/dsfr.module.min.js'
-import '@gouvfr/dsfr/dist/utility/icons/icons.css'
-
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <h1>Hello</h1>
-//   <a href="#" target="_blank" class="fr-link">Pouet</a>
-// `
-
-// setupCounter(document.querySelector('#counter'))
+import "@gouvfr/dsfr/dist/dsfr.min.css";
+import "@gouvfr/dsfr/dist/dsfr.module.min.js";
+import "@gouvfr/dsfr/dist/utility/icons/icons.css";
